1ac2dc8ed44d5fd68a2793f9dab0f23da4594c29
Charles Pelton <charliejp0311@gmail.com>
2020-02-18 20:49:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,15 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+#puts doc.css(".headline-26OIBN").text.strip
+
+
+courses = doc.css(".gridContainer-EnvccM .title-oE5vT4")
+
+courses.each do |course|
+  puts course.text.strip
+end
+
+p doc.css(".gridContainer-EnvccM .title-oE5vT4")[0].children
